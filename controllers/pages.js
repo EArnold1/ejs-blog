22ed720b2df0abc02f50d291dda3af28429860f3
Arnold Emmanuel <arnoldemmanuel15@gmail.com>
2022-07-22 03:47:12 +0100

    Added delete and more functionality

--- controllers/pages.js
+++ controllers/pages.js
@@ -16,6 +16,24 @@ const postsPage = (req, res) => {
     res.render('posts', { title: 'Posts', posts })
 }
 
+// post page
+const getPost = (req, res) => {
+    const { id } = req.params;
+    const post = posts.filter(val => val.id === id)
+    res.render(`post`, { title: `${post.length > 0 ? post[0].title : 'Post'}`, post })
+}
+
+// delete post
+const deletePost = (req, res) => {
+    const { id } = req.params;
+    const index = posts.findIndex(val => val.id === id);
+    posts.splice(index, 1);
+
+    console.log(index)
+    // return res.redirect(`/posts`)
+    res.render(`posts`, { title: 'Post', posts })
+}
+
 // add post logic
 const addPost = (req, res) => {
     const { title, post } = req.body;
@@ -25,6 +43,7 @@ const addPost = (req, res) => {
 
     posts.push(obj)
 
+    console.log(posts)
     res.redirect('/posts')
 }
 
@@ -34,4 +53,4 @@ const notFound = (req, res) => {
     res.render('notfound', { title: 'page not found' })
 }
 
-module.exports = { homePage, addPostPage, addPost, notFound, postsPage }
+module.exports = { homePage, addPostPage, addPost, notFound, postsPage, getPost, deletePost }
--- routes/pages.js
+++ routes/pages.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { homePage, addPostPage, notFound, postsPage } = require('../controllers/pages');
+const { homePage, addPostPage, notFound, postsPage, getPost, deletePost } = require('../controllers/pages');
 
 const router = express.Router();
 
@@ -19,6 +19,18 @@ router.get('/add', addPostPage);
 //desc read posts
 //Public
 router.get('/posts', postsPage);
+
+
+//@route GET /posts
+//desc read posts
+//Public
+router.get('/post/:id', getPost);
+
+
+//@route DELETE /posts
+//desc read posts
+//Public
+router.delete('/post/:id', deletePost);
 
 
 //@route GET /*
